7dc503a872b307998d333adf92d04829f8d4fc68
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-07-19 01:37:16 +0200

    Include rspec style integration test helpers for Capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,20 @@
+## MINITEST TEST LOCATIONS (http://blowmage.com/2012/07/10/announcing-minitest-rails)
+##
+##              Test::Unit                                 MiniTest::Rails
+## Models       test/unit/widget_test.rb                   test/models/widget_test.rb
+## Helpers      test/unit/helpers/widget_helper_test.rb    test/helpers/widget_helper_test.rb
+## Controller   test/functional/widgets_controller_test.rb test/controllers/widgets_controller_test.rb
+## Mailer       test/functional/notifications_test.rb      test/mailers/notifications_test.rb
+## Acceptance   test/integration/user_can_login_test.rb    test/acceptance/user_can_login_test.rb
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 
 require "minitest/rails"
 require 'minitest/autorun'
 require "minitest/rails/capybara"
-# Uncomment if you want awesome colorful output
-require "minitest/pride"
+# require "minitest/pride"
+
 require 'miniskirt'
 require "factories" # If you define your factories in test/factories.rb
 require 'mocha'
@@ -13,9 +22,15 @@ require 'turn'
 
 class MiniTest::Rails::ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class MiniTest::Rails::ActionDispatch::IntegrationTest
+  # include Rails.application.routes.url_helpers
+  # include Capybara::DSL
+  include Capybara::RSpecMatchers
 end
 
 MiniTest::Rails.override_testunit!
