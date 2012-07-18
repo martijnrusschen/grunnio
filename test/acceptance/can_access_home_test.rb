1415a659905f3f2720b6f82fa9894b040f0ee188
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-07-19 01:37:36 +0200

    simple integration test example

---
+++ test/acceptance/can_access_home_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+# To be handled correctly this spec must end with "Acceptance Test"
+describe "CanAccessHome Acceptance Test" do
+  it "must have a content" do
+    visit "/"
+    must_have_content "Silicon Groningen"
+    end
+end
