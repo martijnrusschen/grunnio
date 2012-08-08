46d7ec2020b2da068e904433bab69e2e33d5d10e
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-08-08 16:55:13 +0200

    Devise :confirmable

--- db/migrate/20120808141907_devise_create_accounts.rb
+++ db/migrate/20120808141907_devise_create_accounts.rb
@@ -20,10 +20,10 @@ class DeviseCreateAccounts < ActiveRecord::Migration
       t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
@@ -39,7 +39,7 @@ class DeviseCreateAccounts < ActiveRecord::Migration
 
     add_index :accounts, :email,                :unique => true
     add_index :accounts, :reset_password_token, :unique => true
-    # add_index :accounts, :confirmation_token,   :unique => true
+    add_index :accounts, :confirmation_token,   :unique => true
     # add_index :accounts, :unlock_token,         :unique => true
     # add_index :accounts, :authentication_token, :unique => true
   end
