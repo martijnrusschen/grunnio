b16550f0959ede583259d441eeb94306fdd729e1
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-08-10 00:51:29 +0200

    check if admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,10 @@
 class Admin::ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_admin!
+
+  private
+
+  def authenticate_admin!
+    (current_account.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_account.admin?)
+  end
 end
