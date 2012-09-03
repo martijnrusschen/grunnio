93ef45cc1162bae2f1f2e01d2c3fac4bab356ff0
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-09-03 16:10:57 +0200

    rake db:schema:dump

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120903125428) do
+ActiveRecord::Schema.define(:version => 20120903140843) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                                :default => "",    :null => false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(:version => 20120903125428) do
     t.string   "kvk_number"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
+    t.hstore   "websites"
   end
 
   create_table "companies_people", :id => false, :force => true do |t|
