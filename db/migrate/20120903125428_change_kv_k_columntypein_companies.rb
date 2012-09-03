9afebcf9dd02a770a162a16bbc1da17e938f17a3
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-09-03 15:22:30 +0200

    rollback isn't supported, so remove it from migration

--- db/migrate/20120903125428_change_kv_k_columntypein_companies.rb
+++ db/migrate/20120903125428_change_kv_k_columntypein_companies.rb
@@ -1,9 +1,5 @@
 class ChangeKvKColumntypeinCompanies < ActiveRecord::Migration
-  def up
+  def change
     change_column(:companies, :kvk_number, :string)
-  end
-
-  def down
-    change_column(:companies, :kvk_number, :integer)
   end
 end
