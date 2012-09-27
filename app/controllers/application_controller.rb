eeb72df044a4ced09ed4f767b7708aaf158e0805
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-09-27 16:40:23 +0200

    authentication required for all actions except show & index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with :name => "silgron", :password => "silgron"
+  # http_basic_authenticate_with :name => "silgron", :password => "silgron"
   protect_from_forgery
+  before_filter :authenticate_account!, :except => [:show, :index]
+
+  def authority_forbidden(exception)
+    Authority.configuration.logger.warn(exception.message)
+    redirect_to request.referrer.presence || root_path, :alert => 'You are not authorized to complete that action.'
+  end
 end
