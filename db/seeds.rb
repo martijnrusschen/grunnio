88447fea5045bb035ce59692525ea26c30c15fe1
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-10-04 21:29:48 +0200

    do not load database_cleaner in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 # RocketTag::Tag.create(name: "products")
 # RocketTag::Tag.create(name: "consultancy")
 
-DatabaseCleaner.clean
+DatabaseCleaner.clean unless Rails.env == 'production'
 
 admin = Account.create!(email: 'admin@admin.nl', password: 'silgrongrunnio')
 admin.add_role :admin
