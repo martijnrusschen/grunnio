8c81527607db99cde4e6bcd7ae501e2c6c8a3dcb
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-10-04 16:24:56 +0200

    reconfigured config setup

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,8 +1,8 @@
 SETTINGS = YAML.load(IO.read(Rails.root.join("config", "settings.yml")))
 
 Airbrake.configure do |config|
- config.api_key     = SETTINGS["errbit_token"]
- config.host        = 'errbit.example.com'
+ config.api_key     = SETTINGS["errbit"]["token"]
+ config.host        = SETTINGS["errbit"]["server"]
  config.port        = 80
  config.secure      = config.port == 443
 end
