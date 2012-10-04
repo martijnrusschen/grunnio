a92eae87dfba010896cad69dc8de7a8dfcdffcad
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-10-04 16:39:31 +0200

    refactor config

--- config/initializers/errbit.rb
+++
@@ -1,8 +0,0 @@
-SETTINGS = YAML.load(IO.read(Rails.root.join("config", "settings.yml")))
-
-Airbrake.configure do |config|
- config.api_key     = SETTINGS["errbit"]["token"]
- config.host        = SETTINGS["errbit"]["server"]
- config.port        = 80
- config.secure      = config.port == 443
-end
