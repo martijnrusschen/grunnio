37476bf1d2f1583deeccbec989fe122d77d9449c
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-10-15 22:58:39 +0200

    set default_url_options for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,7 @@ SiliconGroningen::Application.configure do
 
   # Add default url in mails as per Devise setup instructions
 
-  # config.action_mailer.default_url_options = { host: 'grunn.io' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   # config.action_mailer.smtp_settings = {
   #   :address              => "smtp.gmail.com",
   #   :port                 => 587,
