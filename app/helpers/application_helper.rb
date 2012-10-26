90a0bf53b545d1ce8ac27b9807bfc029b532f6c9
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-10-26 11:57:09 +0200

    added current nav class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Helper for .current navigation
+  # http://stackoverflow.com/a/7756320/545017
+
+  def nav_to(link_text, link_path)
+    class_name = current_page?(link_path) ? 'current' : nil
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
