2936c79623598de08377be9e20b6ba912fda3bd8
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-10-29 08:59:44 +0100

    if not persisted redirect to new_account_registration_url

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,16 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  
+
   def all
-    account = Account.from_omniauth(request.env['omniauth.auth'])
-    sign_in_and_redirect account, notice: t('account.signed_in')
+    @account = Account.from_omniauth(request.env['omniauth.auth'])
+    if @account.persisted?
+      sign_in_and_redirect @account, notice: t('account.signed_in')
+    else
+      session["devise.account_attributes"] = request.env["omniauth.auth"]
+      redirect_to new_account_registration_url
+    end
   end
+
   alias_method :linkedin, :all
   alias_method :github, :all
   alias_method :facebook, :all
-  
 end
