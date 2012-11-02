64a4fae2f0c49970e81c1014a7b92f34c3e960e4
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-11-02 21:04:37 +0100

    change default logo

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,8 +23,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-    "http://placehold.it/160x160&text=Logo"
-    # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    # "http://placehold.it/160x160&text=Logo"
+    asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
