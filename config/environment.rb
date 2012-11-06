ed28a95c682628b0ce257f30e4203d0b98184d96
Sebastiaan Pouyet <sebastiaan@robotek.nl>
2012-11-06 14:39:49 +0100

    possible fix for rmagick fail

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ENV['PATH'] = "#{ENV['PATH']}:/usr/local/bin/:/usr/bin"
+
 # Initialize the rails application
 SiliconGroningen::Application.initialize!
